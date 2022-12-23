c59bc0c3c1870f0213a0ecd0e5ec7438a7c5b73b
Vladislav Onishchenko <worldeditor1507@gmail.com>
2022-12-23 12:53:49 +0200

    remove test constructor

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ void main() => runApp(TestApp());
 class TestApp extends StatelessWidget {
   final SpellTextEditingController _controller = SpellTextEditingController();
 
-  TestApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
