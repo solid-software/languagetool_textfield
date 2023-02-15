5bd1c9ebddd224329ecf8e508152817ba0ac5edf
maxymmrn <maxym@i3engineering.com>
2023-02-15 19:03:02 +0200

    added replacement builder for mistake tile

--- lib/presentation/mistake_tile.dart
+++ lib/presentation/mistake_tile.dart
@@ -4,12 +4,17 @@ import 'package:languagetool_textfield/domain/model/mistake.dart';
 const _defaultElevation = 8.0;
 const _defaultWidth = 8.0;
 const _defaultSpacing = 8.0;
+const _defaultBorderRadius = BorderRadius.all(Radius.circular(12));
+
+typedef ReplacementBuilder = Widget Function(String);
 
 class MistakeTile extends StatelessWidget {
   final Mistake mistake;
   final double elevation;
   final double width;
   final double spacing;
+  final BorderRadius? borderRadius;
+  final ReplacementBuilder _replacementBuilder;
 
   const MistakeTile(
     this.mistake, {
@@ -17,13 +22,16 @@ class MistakeTile extends StatelessWidget {
     this.elevation = _defaultElevation,
     this.width = _defaultWidth,
     this.spacing = _defaultSpacing,
-  }) : super(key: key);
+    this.borderRadius = _defaultBorderRadius,
+    ReplacementBuilder? replacementBuilder,
+  })  : _replacementBuilder = replacementBuilder ?? _DefaultReplacement.new,
+        super(key: key);
 
   @override
   Widget build(BuildContext context) => Material(
         elevation: elevation,
         color: Colors.white,
-        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        borderRadius: borderRadius,
         child: SizedBox(
           width: width,
           child: Column(
@@ -34,25 +42,33 @@ class MistakeTile extends StatelessWidget {
               Wrap(
                 runSpacing: spacing,
                 spacing: spacing,
-                children: [
-                  for (final replacement in mistake.replacements)
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        padding: const EdgeInsets.all(8),
-                        backgroundColor: Colors.blueAccent,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(8)),
-                        ),
-                      ),
-                      onPressed: () {
-                        // TODO: Add callback
-                      },
-                      child: Text(replacement),
-                    ),
-                ],
+                children: mistake.replacements
+                    .map(_replacementBuilder)
+                    .toList(growable: false),
               ),
             ],
           ),
         ),
+      );
+}
+
+class _DefaultReplacement extends StatelessWidget {
+  final String replacement;
+
+  const _DefaultReplacement(this.replacement, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => TextButton(
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(8),
+          backgroundColor: Colors.blueAccent,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+        ),
+        onPressed: () {
+          // TODO: Add callback
+        },
+        child: Text(replacement),
       );
 }
