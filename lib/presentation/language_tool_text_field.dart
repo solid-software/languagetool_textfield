da32b764ab8aba49de398ac1adc7a8c51772d547
maxymmrn <maxym@i3engineering.com>
2023-02-15 21:47:28 +0200

    used typedefs in text field too

--- lib/presentation/language_tool_text_field.dart
+++ lib/presentation/language_tool_text_field.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:languagetool_textfield/domain/api/language_check_service.dart';
-import 'package:languagetool_textfield/domain/model/mistake.dart';
 import 'package:languagetool_textfield/domain/model/mistake_type.dart';
 import 'package:languagetool_textfield/presentation/language_check_controller.dart';
 import 'package:languagetool_textfield/presentation/mistake_tile.dart';
 
 class LanguageToolTextField extends StatefulWidget {
   final LanguageCheckService langService;
-  final TextStyle Function(MistakeType?) resolveStyle;
+  final StyleResolver<MistakeType?> resolveStyle;
   final InputDecoration decoration;
-  final Widget Function(Mistake)? mistakeBuilder;
+  final MistakeBuilder? mistakeBuilder;
 
   const LanguageToolTextField({
     Key? key,
