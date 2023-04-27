182fc3dd555098960441efc977adf1bd3a01337f
nazarski <110051407+nazarski@users.noreply.github.com>
2023-04-27 15:34:38 +0300

    Example app added LanguageToolTextField widget

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:languagetool_textfield/languagetool_textfield.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -8,8 +9,25 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  static final LanguageTool _languageTool = LanguageTool();
+  final DebounceLangToolService _debouncedLangService;
+
+  _AppState()
+      : _debouncedLangService = DebounceLangToolService(
+    LangToolService(_languageTool),
+    const Duration(milliseconds: 500),
+  );
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+      child: LanguageToolTextField(
+        langService: _debouncedLangService,
+        style: const TextStyle(),
+        decoration: const InputDecoration(),
+        mistakeBuilder: () {
+          return Container();
+        },
+      ),
+    );
   }
 }
