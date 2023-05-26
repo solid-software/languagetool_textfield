047393578b2604c986acbbcdd167b884719cc171
Dmytro Popov <thedmitryp@ukr.net>
2023-05-26 10:29:09 +0100

    Implemented `ErrorWrapper<T>` monad

---
+++ lib/utils/error_wrapper.dart
@@ -0,0 +1,47 @@
+/// A monad wrapping a non-nullable type [T] alongside the possible
+/// error.
+class ErrorWrapper<T> {
+  /// A wrapped object of type [T].
+  /// If the error has occurred, it will be null.
+  final T? _result;
+
+  /// The error which may have occurred.
+  /// If the error has not occurred, it will ne null.
+  final Object? error;
+
+  /// Returns a wrapped object of type [T], if the result is present.
+  ///
+  /// **Check if [hasResult] before calling this getter.**
+  /// If doesn't have the result, will throw a [StateError].
+  T get result {
+    final res = _result;
+
+    if (res == null) {
+      throw StateError(
+        'No result has been wrapped. Consider checking the [hasResult] '
+        'getter before retrieving the result from the ErrorWrapper.',
+      );
+    }
+
+    return res;
+  }
+
+  /// Returns true if no error has occurred during the API request.
+  bool get hasResult => _result != null;
+
+  /// Creates a new success [ErrorWrapper] with the given [result].
+  const ErrorWrapper.success(this._result) : error = null;
+
+  /// Creates a new error [ErrorWrapper] with the given error.
+  const ErrorWrapper.error(Object this.error) : _result = null;
+
+  /// Maps this [ErrorWrapper] of type [T] to an ErrorWrapper of type [T1] with
+  /// the given [mapper].
+  ErrorWrapper<T1> map<T1>(T1 Function(T) mapper) {
+    if (hasResult) {
+      return ErrorWrapper.success(mapper(result));
+    }
+
+    return ErrorWrapper<T1>.error(error ?? 'Error was not specified');
+  }
+}
