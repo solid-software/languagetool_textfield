f8ead0376dd399c72235fb94fc74373d77eccc2f
solid-danylokhvan <danylo.khvan@solid.software>
2023-06-30 22:15:05 +0300

    comment on the next line

--- lib/utils/extensions/iterable_extension.dart
+++ lib/utils/extensions/iterable_extension.dart
@@ -1,7 +1,7 @@
 /// Extension on Iterable
 extension IterableExtension<T> on Iterable<T> {
-  /// Returns the first element in the iterable that satisfies the given [test] function,
-  /// or `null` if no element satisfies the condition.
+  /// Returns the first element in the iterable that satisfies the given [test] 
+  /// function, or `null` if no element satisfies the condition.
   T? firstWhereOrNull(bool Function(T) test) {
     for (final element in this) {
       if (test(element)) {
