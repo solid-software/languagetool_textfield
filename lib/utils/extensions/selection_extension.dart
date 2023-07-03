a7c6337339bf8ed02dcde8fb9870ce7497b21210
solid-danylokhvan <danylo.khvan@solid.software>
2023-07-03 17:02:43 +0300

    code formatting

--- lib/utils/extensions/selection_extension.dart
+++ lib/utils/extensions/selection_extension.dart
@@ -3,12 +3,11 @@ import 'package:languagetool_textfield/domain/mistake.dart';
 
 /// Extension methods for the TextSelection class
 extension SelectionExtension on TextSelection {
-
-  /// Checks if the base offset of the TextSelection 
+  /// Checks if the base offset of the TextSelection
   /// is before the mistake offset
   bool isBaseBeforeMistake(Mistake mistake) => base.offset < mistake.offset;
 
-  /// Checks if the base offset of the TextSelection 
+  /// Checks if the base offset of the TextSelection
   /// is before or at the mistake offset
   bool isBaseBeforeOrAtMistake(Mistake mistake) =>
       base.offset <= mistake.offset;
@@ -18,7 +17,7 @@ extension SelectionExtension on TextSelection {
       (end > mistake.offset && end <= mistake.endOffset) ||
       (start > mistake.offset && start <= mistake.endOffset);
 
-  /// Checks if the base offset of the TextSelection 
+  /// Checks if the base offset of the TextSelection
   /// is within the mistake boundaries
   bool isBaseWithinAndAtMistakeBoundaries(Mistake mistake) =>
       base.offset >= mistake.offset && base.offset <= mistake.endOffset;
