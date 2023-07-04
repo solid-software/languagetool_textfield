61966e3a4e1d02eaf334d16d4b55d88bf6d185b3
solid-danylokhvan <danylo.khvan@solid.software>
2023-07-04 20:17:55 +0300

    rm suffix

--- lib/implementations/keep_latest_response_service.dart
+++ lib/implementations/keep_latest_response_service.dart
@@ -1,16 +1,16 @@
-import 'package:async/async.dart' as async;
+import 'package:async/async.dart';
 
 /// A service that executes asynchronous operations and ensures that only
 /// the results of the latest operation are returned.
 class KeepLatestResponseService {
-  async.CancelableOperation<dynamic>? _currentOperation;
+  CancelableOperation<dynamic>? _currentOperation;
 
   /// Executes the latest operation and returns its result.
   /// Only the results of the most recent operation are returned,
   /// discarding any previous ongoing operations.
   Future<T?> processLatestOperation<T>(Future<T> Function() action) async {
     await _currentOperation?.cancel();
-    _currentOperation = async.CancelableOperation<T>.fromFuture(action());
+    _currentOperation = CancelableOperation<T>.fromFuture(action());
 
     return _currentOperation?.value as Future<T>?;
   }
